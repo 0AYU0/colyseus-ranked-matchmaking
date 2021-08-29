fa809358ddbb86ff0cd81327591eb8ccae5c5e7f
0AYU0 <awy001@ucsd.edu>
2021-08-28 23:22:55 -0700

    added-matchmaking-function

--- src/RankedLobbyRoom.ts
+++ src/RankedLobbyRoom.ts
@@ -14,12 +14,12 @@ interface ClientStat {
 }
 
 interface Patient extends ClientStat {
-  availability: {start: Date; end: Date;};
+  availability: { start: Date; end: Date; };
   bestFit: Caregiver[];
 }
 
 interface Caregiver extends ClientStat {
-  availability: {start: Date; end: Date;}[];
+  availability: { start: Date; end: Date; }[];
 }
 
 export class RankedLobbyRoom extends Room {
@@ -109,14 +109,14 @@ export class RankedLobbyRoom extends Room {
         availability: options.nextAvailableDays,
       });
     }
-    console.log(this.patientStats);
   }
 
   redistributeGroups() {
     this.patientStats.forEach((patient) => {
       this.caregiverStats.forEach((caregiver) => {
         const rank = this.genRankUsers(patient, caregiver);
-        if (rank > 25) {
+        console.log(rank);
+        if (rank > 10) {
           if (!patient.bestFit.some(elem => elem === caregiver)) {
             patient.bestFit.push(caregiver);
           }
@@ -126,8 +126,48 @@ export class RankedLobbyRoom extends Room {
     this.checkGroupsReady();
   }
 
-  genRankUsers(patient: ClientStat, caregiver: ClientStat) {
-    return 50;
+  genRankUsers(patient: Patient, caregiver: Caregiver) {
+    const availabilityScore = this.calculateAvailability(patient, caregiver); //overlap of availability in minutes
+    const distanceScore = this.calculateDistance(patient, caregiver); // distance between them in miles
+    console.log(availabilityScore);
+    console.log(distanceScore);
+    return 100 * this.calculateAvailability(patient, caregiver) / this.calculateDistance(patient, caregiver);
+  }
+
+  calculateAvailability(patient: Patient, caregiver: Caregiver) {
+    const currentDate = new Date();
+    const patientAvailabilityStart = new Date(currentDate.toDateString() + " " + new Date(patient.availability.start).toLocaleTimeString())
+      .setDate(new Date(currentDate).getDate() + ((7 - new Date(currentDate).getDay() + new Date(patient.availability.start).getDay()) % 7)); // takes weekday that patient is available, converts it to nearest day from current day
+    const patientAvailabilityEnd = new Date(currentDate.toDateString() + " " + new Date(patient.availability.end).toLocaleTimeString())
+      .setDate(new Date(currentDate).getDate() + ((7 - new Date(currentDate).getDay() + new Date(patient.availability.end).getDay()) % 7));
+    const overlappingTimes = caregiver.availability.map(startEnd => {
+      const start = startEnd.start;
+      const end = startEnd.end;
+      const overlap = Math.max(0, Math.min(new Date(end).getTime(), patientAvailabilityEnd) - Math.max(new Date(start).getTime(), patientAvailabilityStart)); // finds overlap in timing based on all caregiver availabilities
+      return overlap / 60000;
+    })
+    return overlappingTimes.reduce((num1, num2) => num1 + num2, 0); // sums up overlapping times
+  }
+
+  calculateDistance(patient: Patient, caregiver: Caregiver) {
+    const patientLongtiude = patient.location.longitude;
+    const patientLatitude = patient.location.latitude;
+    const caregiverLongtiude = caregiver.location.longitude;
+    const caregiverLatitude = caregiver.location.latitude;
+
+    const radiusMiles = 3963.1676;
+
+    const radianPatientLatitude = patientLatitude * Math.PI / 180;
+    const radianCaregiverLatitude = caregiverLatitude * Math.PI / 180;
+
+    const angleLatitude = (patientLatitude - caregiverLatitude) * Math.PI / 180;
+    const angleLongitude = (patientLongtiude - caregiverLongtiude) * Math.PI / 180;
+
+    const a = Math.sin(angleLatitude / 2) * Math.sin(angleLatitude / 2) +
+      Math.cos(radianCaregiverLatitude) * Math.cos(radianPatientLatitude) *
+      Math.sin(angleLongitude) * Math.sin(angleLongitude);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return radiusMiles * c;
   }
 
   checkGroupsReady() {
